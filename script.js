ead23e7b72acff56e30ef59be58ce88993803a57
sabrinaDev28 <sabrinaprofissional2023@gmail.com>
2024-12-03 14:25:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para redirecionar para a próxima página
+function goToGallery() {
+    window.location.href = "galeria.html"; // Nome do arquivo da próxima página
+  }
+
+// Função para virar a carta e mostrar a frase
+function flipCard(card) {
+  card.classList.toggle('flip'); // Adiciona ou remove a classe 'flip' que ativa o efeito de virada
+}
